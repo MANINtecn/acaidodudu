f8009c66041adea2cb477076f5e52e9427c06d45
MANINTECX <manintecn@gmail.com>
2026-08-19 15:43:09 -0300

    fix: corrige base path para ./ no vite.config.ts resolvendo tela branca e configura build universal x64/ia32 + portable no package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import legacy from '@vitejs/plugin-legacy'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [
     react(),
     legacy({
